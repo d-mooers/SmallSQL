b6015e3f3fc49247f443e104ab05f2930be34d71
Remy <remywolf.r@gmail.com>
2022-05-10 16:40:18 -0700

    added CommandRecommendIndex class, no functionality

---
+++ src/main/java/smallsql/database/CommandRecommendIndex.java
@@ -0,0 +1,21 @@
+package smallsql.database;
+
+import java.sql.*;
+
+/* Called via RECOMMEND INDEX; */
+public class CommandRecommendIndex extends Command {
+
+    private IndexRecommender rec;
+
+    CommandRecommendIndex(Logger log, IndexRecommender rec) {
+        super(log);
+        this.rec = rec;
+    }
+
+    @Override
+    void executeImpl(SSConnection con, SSStatement st) throws Exception {
+        // check if session?
+        // TODO: do this in CLI, not this class
+        System.out.println(rec.getRecommendedIndexes());
+    }
+}
