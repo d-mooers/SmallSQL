9f4c97423c3b42c155938bcd86a4d8e8f86c5302
Dylan Mooers <dylmooers@gmail.com>
2022-06-02 17:32:40 -0700

    tests

--- src/main/java/smallsql/database/IndexRecommenderRelativeFrequency.java
+++ src/main/java/smallsql/database/IndexRecommenderRelativeFrequency.java
@@ -8,14 +8,15 @@ public class IndexRecommenderRelativeFrequency extends IndexRecommender {
     
     int totalSelections;
     int totalJoins;
+    double threshold;
 
     PriorityQueue<Field> pq;
 
     public IndexRecommenderRelativeFrequency(SSConnection con, ArrayList<Field> fields) {
         super(con, fields);
         pq = new PriorityQueue<Field>((f1, f2) -> {
-            double relativeSelections_1 = (f1.getSelections() / (double) this.totalSelections) + (f1.getJoins() / (double) this.totalJoins);
-            double relativeSelections_2 = (f1.getSelections() / (double) this.totalSelections) + (f1.getJoins() / (double) this.totalJoins);
+            double relativeSelections_1 = calculateRelativeFrequency(f1);
+            double relativeSelections_2 = calculateRelativeFrequency(f2);
     
             if (relativeSelections_1 > relativeSelections_2) return -1;
             return 1;
@@ -33,7 +34,9 @@ public class IndexRecommenderRelativeFrequency extends IndexRecommender {
         });
     }
 
-
+    private double calculateRelativeFrequency(Field f) {
+        return (f.getSelections() / (double) this.totalSelections) + (f.getJoins() / (double) this.totalJoins);
+    }
 
     public ArrayList<String[]> recommendIndex() {
         // tracks number of deletions and insertions per table
@@ -42,7 +45,7 @@ public class IndexRecommenderRelativeFrequency extends IndexRecommender {
             totalJoins += field.getJoins();
         }
         for (Field field : this.fields) {
-            pq.offer(field);
+            if (calculateRelativeFrequency(field) > 0) pq.offer(field);
         }
         while(!pq.isEmpty()) {
             Field field = pq.poll();
